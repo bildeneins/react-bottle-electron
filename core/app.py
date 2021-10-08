ccda615f645e0c804579ee03fb4bd60319d752f4
stonesstones <ku292stones@gmail.com>
2021-10-08 16:48:15 +0900

    動画

--- core/app.py
+++ core/app.py
@@ -1,22 +1,27 @@
+import time
+import bottle
+import cv2
+app = bottle.Bottle()
+def gen():
+    
+    cam = cv2.VideoCapture(0)
+    while True:
+        # 動画から1フレーム分の画像を読み込み
+        ret_val, image = cam.read()
+        if not ret_val:
+            break
+        # jpg形式に変換
+        flag, frame = cv2.imencode('.jpg', image)
+        yield b'--frame\r\n' + b'Content-Type: image/jpeg\r\n\r\n' + bytearray(frame) + b'\r\n\r\n'
+        # 連続再生されるのでwaitを入れる
+        time.sleep(1/60)
+        
+@app.route('/')
+def main():
+    return bottle.static_file('index.html', root='./')
 
-from bottle import route, run, template
-
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
-
-run(host='localhost', port=8080)
-1
-2
-3
-4
-5
-6
-7
-from bottle import route, run, template
- 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
- 
-run(host='localhost', port=8080)
+@app.route('/video_feed')
+def video_feed():
+    bottle.response.content_type = 'multipart/x-mixed-replace;boundary=frame'
+    return gen()
+app.run(host='localhost', port=8080, reloader=True, debug=True)
--- gui/src/App.js
+++ gui/src/App.js
@@ -4,20 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<header className="App-header">*/}
+      {/*  <p>*/}
+      {/*    Edit <code>src/App.js</code> and save to reload.*/}
+      {/*  </p>*/}
+      {/*  <a*/}
+      {/*    className="App-link"*/}
+      {/*    href="https://reactjs.org"*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    Learn React*/}
+      {/*  </a>*/}
+      {/*</header>*/}
+      <div className="App-img">
+          <h1>daaaaa</h1>
+          <img src="http://localhost:8080/video_feed"  width="900" height="500"/>
+      </div>
     </div>
   );
 }
