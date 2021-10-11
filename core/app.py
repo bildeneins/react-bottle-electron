1348a0f60b13d9dbde0738c3e5fd664d4e30a44f
stonesstones <ku292stones@gmail.com>
2021-10-11 10:46:16 +0900

    getとpostの途中

--- core/app.py
+++ core/app.py
@@ -1,7 +1,11 @@
 import time
-import bottle
+# import bottle
+from bottle import hook, response, route, run, app, static_file, HTTPResponse, request
 import cv2
-app = bottle.Bottle()
+import json
+ 
+
+# app = bottle.Bottle()
 def gen():
     
     cam = cv2.VideoCapture(0)
@@ -15,13 +19,41 @@ def gen():
         yield b'--frame\r\n' + b'Content-Type: image/jpeg\r\n\r\n' + bytearray(frame) + b'\r\n\r\n'
         # 連続再生されるのでwaitを入れる
         time.sleep(1/60)
-        
-@app.route('/')
-def main():
-    return bottle.static_file('index.html', root='./')
 
-@app.route('/video_feed')
+@route('/')
+def main():
+    return static_file('index.html', root='./')
+
+@route('/video_feed')
 def video_feed():
-    bottle.response.content_type = 'multipart/x-mixed-replace;boundary=frame'
+    response.content_type = 'multipart/x-mixed-replace;boundary=frame'
     return gen()
-app.run(host='localhost', port=8080, reloader=True, debug=True)
+
+@route('/get', method='OPTIONS')
+def support_cors():
+    res = HTTPResponse(status=200, body=None)
+    res.set_header('Access-Control-Allow-Origin', 'http://localhost:3000/')
+    return res
+
+@route('/get', method='GET') 
+def get():
+    print(request.headers)
+    res = HTTPResponse(status=200, body=json.dumps("get ok"))
+    res.set_header('Content-Type', 'application/json')
+    res.set_header('Access-Control-Allow-Origin', 'http://localhost:3000/')
+    return res
+
+@route('/post', method='OPTIONS')
+def support_cors():
+    res = HTTPResponse(status=200, body=None)
+    res.set_header('Access-Control-Allow-Origin', 'http://localhost:3000/')
+    return res          
+@route('/post',method="POST")
+def post():
+    data = { "post ok" }
+    res = HTTPResponse(status=200, body=json.dumps(data))
+    res.set_header('Content-Type', 'application/json')
+    res.set_header('Access-Control-Allow-Origin', '*')
+    return res
+
+run(host='localhost', port=8080, reloader=True, debug=True)
--- gui/src/App.js
+++ gui/src/App.js
@@ -1,28 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
 
 function App() {
+    function onClicked_get() {
+        console.log("send get request")
+        axios.get("http://localhost:8080/get",{
+            headers: {
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin': "http://localhost:8080/",
+            }
+        })
+            .then(function (res) {
+                console.log("got get response")
+                alert(res.data)
+        })
+            .catch(error => {
+            console.log(error);
+        });
+    }
+    function onClicked_post() {
+        console.log("send post request")
+        axios.post("http://localhost:8080/post", "post data",{
+            headers: {
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin': "http://localhost:8080/",
+            }
+        })
+            .then(function (res) {
+                console.log("got post response")
+                alert(res.data)
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
   return (
     <div className="App">
-      {/*<header className="App-header">*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.js</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
-      {/*</header>*/}
       <div className="App-img">
           <h1>daaaaa</h1>
-          <img src="http://localhost:8080/video_feed"  width="900" height="500"/>
+          <button onClick={onClicked_get}>
+              get request button
+          </button>
+          <button onClick={onClicked_post}>
+              post request button
+          </button>
+          {/*<img src="http://localhost:8080/video_feed"  width="900" height="500"/>*/}
       </div>
     </div>
   );
 }
 
 export default App;
+
